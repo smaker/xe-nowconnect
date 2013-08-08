07c276a2b88ee7017acc0f4e9c7902d36365f0fb
SMaker <dowon2308@gmail.com>
2013-08-08 20:28:02 +0900

    Remove Unnecessary Code

--- nowconnect/nowconnect.class.php
+++ nowconnect/nowconnect.class.php
@@ -86,40 +86,6 @@ class nowconnect extends ModuleObject
 	}
 }
 
-function _unserializeSession($val) {
-	$result = array();
-
-	// prefixing with semicolon to make it easier to write the regular expression
-	$val = ';' . $val;
-
-	// regularexpression to find the keys
-	$keyreg = '/;([^|{}"]+)\|/';
-
-	// find all keys
-	$matches = array();
-	preg_match_all($keyreg, $val, $matches);
-
-	// only go further if we found some keys
-	if(isset($matches[1]))
-	{
-		$keys = $matches[1];
-
-		// find the values by splitting the input on the key regular expression
-		$values = preg_split($keyreg, $val);
-
-		// unshift the first value since it's always empty (due to our semicolon prefix)
-		if(count($values) > 1)
-		{
-			array_shift($values);
-		}
-
-		// combine the $keys and $values
-		$result = @array_combine($keys, $values);
-	}
-
-	return $result;
-}
-
 function getUserAgentInfo($user_agent = null)
 {
 	if(!$user_agent)
